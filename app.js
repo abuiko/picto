5f64ee064f1881eb5733009a3d7388577ad4b523
Alena Buiko <abuiko.ma@gmail.com>
2022-06-10 15:21:12 -0400

    add more photos when click btn

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const key = config.MY_API_KEY;
 const gallery = document.querySelector(".gallery")
 const searchInput = document.querySelector(".search-input")
 const form = document.querySelector(".search-form")
+const more = document.querySelector(".more")
 
 let searchValue;
+let page = 1
+let fetchLink;
+
 // event listeners
 searchInput.addEventListener("input", updateInput)
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     searchPhotos(searchValue)
 })
+more.addEventListener('click', loadMore)
 
 function updateInput(e) {
     searchValue = e.target.value
@@ -34,28 +39,43 @@ function generatePhotos(data) {
         const galleryImg = document.createElement('div')
         galleryImg.classList.add('gallery-img')
         galleryImg.innerHTML = `
+            
+            <div class="gallery-info">
+                <p>${photo.photographer}</p>
+                <a href=${photo.src.original} target="_blank">Download</a>
+            </div>
             <img src=${photo.src.large}></img>
-            <p>${photo.photographer}</p>
+            
         `
         gallery.appendChild(galleryImg)
     });
 }
 
 async function curatedPhotos() {
-    const data = await fetchApi("https://api.pexels.com/v1/curated?per_page=15&page=1")
+    fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1"
+    const data = await fetchApi(fetchLink)
     generatePhotos(data)
 
 }
 
 async function searchPhotos(query) {
     clear()
-    const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`)
+    fetchLink = `https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`
+    const data = await fetchApi(fetchLink)
     generatePhotos(data)
-
 }
 
 function clear() {
     gallery.innerHTML = ''
+    searchInput.value = ""
 }
-// searchPhotos()
+
+async function loadMore() {
+    page++
+    const updateLink = fetchLink.slice(0, -1) + page
+    const dataFetch = await fetchApi(updateLink)
+    generatePhotos(dataFetch)
+}
+
+
 curatedPhotos()
