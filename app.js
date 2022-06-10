ad6d3798f36fea0b995b382c5bcc8ee3cf16002b
Alena Buiko <abuiko.ma@gmail.com>
2022-06-10 12:00:13 -0400

    serch for images

--- app.js
+++ app.js
@@ -2,12 +2,48 @@ const key = config.MY_API_KEY;
 
 const gallery = document.querySelector(".gallery")
 const searchInput = document.querySelector(".search-input")
-const submitBtn = document.querySelector(".submit-btn")
+const form = document.querySelector(".search-form")
+
+let searchValue;
+// event listeners
+searchInput.addEventListener("input", updateInput)
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    searchPhotos(searchValue)
+})
 
 
+function updateInput(e) {
+    searchValue = e.target.value
+}
 async function curatedPhotos() {
     const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15&page=1", {
         method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: key
+        }
+    })
+    const data = await dataFetch.json()
+
+    data.photos.forEach(photo => {
+        const galleryImg = document.createElement('div')
+        galleryImg.classList.add('gallery-img')
+        galleryImg.innerHTML = `
+            <img src=${photo.src.large}></img>
+            <p>${photo.photographer}</p>
+        `
+        gallery.appendChild(galleryImg)
+    });
+}
+
+// "https://api.pexels.com/v1/search?query=nature&per_page=1"
+
+async function searchPhotos(query) {
+    const dataFetch = await fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`, {
+
+        method: "GET",
+
         headers: {
             Accept: "application/json",
             Authorization: key
@@ -24,6 +60,7 @@ async function curatedPhotos() {
         `
         gallery.appendChild(galleryImg)
     });
-}
 
+}
+// searchPhotos()
 curatedPhotos()
